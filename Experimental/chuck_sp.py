afda4f576a183fb53117eedca6b32f4fc6bf2e01
eWizardII <electronicwizard14@gmail.com>
2010-12-04 03:38:48 -0500

    Code time 75.960938 seconds - run across 5 APIs.

--- Experimental/chuck_sp.py
+++ Experimental/chuck_sp.py
@@ -12,7 +12,7 @@ def norris():
     su = 0
     ou = 0
     i  = 1
-    while (i < 20):
+    while (i < 100):
         try:
             urlv = "http://twitter1-ewizardii.apigee.com/1/statuses/user_timeline/"+ str(i) + ".json?count=200&trim_user=true"
             source = urllib2.urlopen(urlv)
@@ -25,45 +25,45 @@ def norris():
             urlv5 = "http://twitter5-ewizardii.apigee.com/1/statuses/user_timeline/"+ str(i+4) + ".json?count=200&trim_user=true"
             source5 = urllib2.urlopen(urlv5)
             i = i + 5
-            soup = BeautifulSoup(source)
-            output = str(soup)
-
-            fileObj = open("/media/Cache/Twitter/json_scrap/temp.json","w")
-            fileObj.write(output)
-            fileObj.close()
-
-            json_data = open("/media/Cache/Twitter/json_scrap/temp.json")
-
-            data = json.load(json_data)
-
-            i = 0
-            twitter_data = {}
-            for record in data:
-                twitter_data[i]                                 = {}
-                twitter_data[i]['created_at']                   = record['created_at']
-                twitter_data[i]['favorited']                    = record['favorited']
-                twitter_data[i]['id_str']                       = record['id_str']
-                twitter_data[i]['in_reply_to_status_id_str']    = record['in_reply_to_status_id_str']
-                twitter_data[i]['in_reply_to_user_id_str']      = record['in_reply_to_user_id_str']
-                twitter_data[i]['retweet_count']                = record['retweet_count']
-                twitter_data[i]['retweeted']                    = record['retweeted']
-                twitter_data[i]['text']                         = record['text']
-                twitter_data[i]['user']                         = record['user']
-                record_id                                       = record['user']
-                twitter_data[i]['id_str']                       = record_id['id_str']
-                i = i + 1
-
-            with open('/media/Cache/Twitter/junk/' + str(record_id['id_str']) + '.json', mode='w') as f:
-                json.dump(twitter_data, f, indent=2, encoding='utf-8')
-
-            f_in = open('/media/Cache/Twitter/junk/' + str(record_id['id_str']) + '.json', 'rb')
-            f_out = gzip.open('/media/Cache/Twitter/junk/' + str(record_id['id_str']) + '.json.gz', 'wb')
-            f_out.writelines(f_in)
-            f_out.close()
-            f_in.close()
-            os.remove('/media/Cache/Twitter/junk/' + str(record_id['id_str']) + '.json')
-            ou = ou + 1
-            tu = tu + 1
+##            soup = BeautifulSoup(source)
+##            output = str(soup)
+##
+##            fileObj = open("B:/Cache/Twitter/json_scrap/temp.json","w")
+##            fileObj.write(output)
+##            fileObj.close()
+##
+##            json_data = open("B:/Cache/Twitter/json_scrap/temp.json")
+##
+##            data = json.load(json_data)
+##
+##            i = 0
+##            twitter_data = {}
+##            for record in data:
+##                twitter_data[i]                                 = {}
+##                twitter_data[i]['created_at']                   = record['created_at']
+##                twitter_data[i]['favorited']                    = record['favorited']
+##                twitter_data[i]['id_str']                       = record['id_str']
+##                twitter_data[i]['in_reply_to_status_id_str']    = record['in_reply_to_status_id_str']
+##                twitter_data[i]['in_reply_to_user_id_str']      = record['in_reply_to_user_id_str']
+##                twitter_data[i]['retweet_count']                = record['retweet_count']
+##                twitter_data[i]['retweeted']                    = record['retweeted']
+##                twitter_data[i]['text']                         = record['text']
+##                twitter_data[i]['user']                         = record['user']
+##                record_id                                       = record['user']
+##                twitter_data[i]['id_str']                       = record_id['id_str']
+##                i = i + 1
+##
+##            with open('B:/Cache/Twitter/junk/' + str(record_id['id_str']) + '.json', mode='w') as f:
+##                json.dump(twitter_data, f, indent=2, encoding='utf-8')
+##
+##            f_in = open('B:/Cache/Twitter/junk/' + str(record_id['id_str']) + '.json', 'rb')
+##            f_out = gzip.open('B:/Cache/Twitter/junk/' + str(record_id['id_str']) + '.json.gz', 'wb')
+##            f_out.writelines(f_in)
+##            f_out.close()
+##            f_in.close()
+##            os.remove('B:/Cache/Twitter/junk/' + str(record_id['id_str']) + '.json')
+##            ou = ou + 1
+##            tu = tu + 1
         except:
             i = i + 1
             su = su + 1
