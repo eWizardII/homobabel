d0d37e82998ed76ef0764de809003095a50815a4
eWizardII <electronicwizard14@gmail.com>
2011-01-04 07:12:46 -0500

    Updated with correct formating and file handling.

--- eagle_prefetch.py
+++ eagle_prefetch.py
@@ -26,9 +26,10 @@ def lvl1():
     ## Thread loop - birdofprey
 
     class birdofprey(Thread):
-        def __init__ (self,ip,api_name,oauth_token,oauth_token_secret):
+        def __init__ (self,ip,id,api_name,oauth_token,oauth_token_secret):
             Thread.__init__(self)
             self.ip                 = ip
+            self.id                 = id
             self.api_name           = api_name
             self.oauth_token        = oauth_token
             self.oauth_token_secret = oauth_token_secret
@@ -51,23 +52,36 @@ def lvl1():
             ## Grab the seeds (i.e. the top 100 Twitter users ranging from @ladygaga - @womensweardaily
 
             i               = 0
+            twitter_data    = {}
             for null in range(0,1):
                 while True:
                     try:
                         for friends in Cursor(api.followers_ids,screen_name=self.ip).items():
+                            twitter_data[i]['fu']               = self.ip
+                            twitter_data[i]['su']               = friends
                             i = i + 1
-                            with open('B:/Twitter/topusers.csv', mode='a') as a_file:
-                                new_i  = ',' + str(friends)
-                                a_file.write(new_i)
                     except tweepy.TweepError, e:
                         print "ERROR on " + str(self.ip) + " Reason: ", e
-                        with open('B:/Twitter/errors.txt', mode='a') as a_file:
+                        with open('C:/Twitter/errors.txt', mode='a') as a_file:
                             new_ii = "ERROR on " + str(self.ip) + " Reason: " + str(e) + "\n"
                             a_file.write(new_ii)
                     break
 
+            ## Display the status of the project
+
+            with open('C:/Twitter/user_' + str(self.id) + '.json', mode='w') as f:
+                    json.dump(twitter_data, f, indent=2, encoding='utf-8')
+
             print str(i) + " users found for " + str(self.ip) + " \n"
             print "Completed"
+
+            ## Log the status of the project
+
+            with open('C:/Twitter/pre_status.txt', mode='a') as a_file:
+                new_iii = str(i) + " users found for " + str(self.ip) + " \n"
+                new_iv  = "Completed"
+                a_file.write(new_iii)
+                a_file.write(new_iv)
 
         def join(self,timeout=None):
             Thread.join(self, timeout)
@@ -79,6 +93,7 @@ def lvl1():
     api_name    = 1
     json_data2  = open("B:/Twitter/commodore.json")
     data2       = json.load(json_data2)
+    id          = -1
 
     json_data3  = open("B:/Twitter/top100.json")
     data3       = json.load(json_data3)
@@ -88,9 +103,10 @@ def lvl1():
     for host in range(0,67):
         ## User 1 in API
         ip                  = data3[host]['screen_name']
+        id                  = id + 1
         oauth_token         = data2[0][str(delta)]['oauth_token']
         oauth_token_secret  = data2[0][str(delta)]['oauth_token_secret']
-        urlv                = birdofprey(ip,api_name,oauth_token,oauth_token_secret)
+        urlv                = birdofprey(ip,id,api_name,oauth_token,oauth_token_secret)
         urlv.start()
         source.append(urlv)
         print ip
@@ -98,9 +114,10 @@ def lvl1():
     for host in range(67,134):
         ## User 2 in API
         ip                  = data3[host]['screen_name']
+        id                  = id + 1
         oauth_token         = data2[0][str(delta+1)]['oauth_token']
         oauth_token_secret  = data2[0][str(delta+1)]['oauth_token_secret']
-        urlv                = birdofprey(ip,api_name,oauth_token,oauth_token_secret)
+        urlv                = birdofprey(ip,id,api_name,oauth_token,oauth_token_secret)
         urlv.start()
         source.append(urlv)
         print ip
@@ -110,19 +127,21 @@ def lvl1():
     for host in range(134,168):
         ## User 3 in API
         ip                  = data3[host]['screen_name']
+        id                  = id + 1
         oauth_token         = data2[0][str(delta+2)]['oauth_token']
         oauth_token_secret  = data2[0][str(delta+2)]['oauth_token_secret']
-        urlv                = birdofprey(ip,api_name2,oauth_token,oauth_token_secret)
+        urlv                = birdofprey(ip,id,api_name2,oauth_token,oauth_token_secret)
         urlv.start()
         source.append(urlv)
         print ip
 
-    for host in range(168,194):
+    for host in range(168,193):
         ## User 3 in API
         ip                  = data3[host]['screen_name']
+        id                  = id + 1
         oauth_token         = data2[0][str(delta+2)]['oauth_token']
         oauth_token_secret  = data2[0][str(delta+2)]['oauth_token_secret']
-        urlv                = birdofprey(ip,api_name2,oauth_token,oauth_token_secret)
+        urlv                = birdofprey(ip,id,api_name2,oauth_token,oauth_token_secret)
         urlv.start()
         source.append(urlv)
         print ip
