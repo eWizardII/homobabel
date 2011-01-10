d838120cd9760d025eda99cd4d69aa829e0ba76a
eWizardII <electronicwizard14@gmail.com>
2011-01-10 05:11:05 -0500

    Program fixed now spends less time trying to find files in directory, fully commented to track program progress as it runs.

--- Stripper.py
+++ Stripper.py
@@ -2,29 +2,48 @@ import time
 import json
 import string
 import unicodedata
-import os.path
+import os
 
 def filetxt():
     word_freq = {}
     word_occ  = {}
+    word_supp = {}
+    word_stat = {}
+    word_users_behind_word = {}
     lvl1      = []
     lvl2      = []
     total_t   = 0
     users     = 0
     text      = []
+    foll      = []
+    stat      = []
     word_list = []
-    
-    for l in range(2500,234980613):
+    path="C:\Twitter\json_"
+    dirList=os.listdir(path)
+
+    print "Directory Listing Complete"
+    errors = 0
+    for fname in dirList:
         # Open File
-        if os.path.exists("C:/Twitter/json_/user_" + str(l) + "_id_" + str(l) + ".json") == True:
-            with open("C:/Twitter/json_/user_" + str(l) + "_id_" + str(l) + ".json", "r") as f:
+        try:
+            with open("C:/Twitter/json_/" + str(fname), "r") as f:
                 text_f = json.load(f)
-                users = users + 1
-                for i in range(len(text_f)):
-                    text.append(text_f[str(i)]['text'])
-                    total_t = total_t + 1
-        else:
+                stat_v = text_f['0']['statuses_count']
+                foll_v = text_f['0']['followers_count']
+                if (stat_v > 10) and (foll_v > 10):
+                    users = users + 1
+                    for i in range(len(text_f)):
+                        text.append(text_f[str(i)]['text'])
+                        stat.append(stat_v)
+                        foll.append(foll_v)
+                        total_t = total_t + 1
+                else:
+                    pass
+        except:
+            errors += errors
             pass
+        
+    print "Running Filter"
 
     # Filter
     occ = 0
@@ -32,6 +51,8 @@ def filetxt():
     for i in range(len(text)):
         s = text[i] # Sample string
         occ_t = s.count('RT') + s.count('@')
+        supp_t = foll[i]
+        stat_t = stat[i]
         occ += occ_t
         s = s.encode('utf-8')
         out = s.translate(string.maketrans("",""), string.punctuation)
@@ -41,7 +62,12 @@ def filetxt():
         for word in word_lists:
             word_freq[word] = word_freq.get(word, 0) + 1
             word_occ[word]  = word_occ.get(word, 0) + occ_t
-            
+            word_supp[word] = word_supp.get(word, 0) + supp_t
+            word_stat[word] = word_stat.get(word, 0) + stat_t
+            word_users_behind_word[word] = word_users_behind_word.get(word, 0) + 1 
+
+    print "Running Analysis"
+    
     keys = word_freq.keys()
 
     numbo = range(1,len(keys)+1)
@@ -59,12 +85,14 @@ def filetxt():
             lvl1.append(word_list[i])
             lvl2.append(word_list[i+1])
 
-    out_f = open("matlab.txt", 'w')
-
+    ## Write data for analysis
+    print "Saving Analysis"
+    
     mo = open("matlab.txt", "wb")
     
     for row in word_occ:
-        mo.write(str(row.encode('utf_8'))+" "+str(word_occ[row])+'\r\n')
+        mo.write(str(row.encode('utf_8'))+" "+str(word_occ[row])+" "+str(word_supp[row]/word_users_behind_word[row])+" "+str(word_stat[row]/word_users_behind_word[row])+'\r\n')
+        #mo.write(str(row.encode('utf_8'))+" "+str(word_occ[row])+'\r\n')
 
     mo.close()
     
@@ -92,6 +120,7 @@ def filetxt():
     print "Total Tweets: " + str(total_t)
     print "Total Users:  " + str(users)
     print "Total Occ: " + str(occ)
+    print "Total Errors: " + str(errors)
     
     print "Done!"
 
